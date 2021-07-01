6a4d6ab8acb9ed2235457095498f0f457d19eb45
Vidhi Gupta <42618900+vidhigupta9@users.noreply.github.com>
2021-07-02 03:54:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
-import model as m
+import matplotlib.pyplot as plt
 
 
 app = Flask(__name__)
@@ -16,12 +16,27 @@ def home():
 def predict():
     '''
     For rendering results on HTML GUI
-    '''
-    d = request.form['experience']
-    prediction = result.predict(d)
-    return render_template('index.html', prediction_text='Consumption should be {}\n'.format(np.expm1(prediction.values)))
-  
+    '''    
+    date_initial = request.form['date_initial']
+    date_final = request.form['date_final']
+    prediction = result.predict(start=date_initial,end=date_final)      
+    prediction = np.expm1(prediction)
+
+    monthly = prediction.resample('m').sum()
+    monthly = pd.DataFrame(monthly,columns=['unit'])
+    monthly.index = pd.to_datetime(monthly.index).month
+
+    monthly.plot(kind='bar',colormap='rainbow',width=0.70)
+    plt.xticks(rotation=90)
+    plt.xlabel('Months')
+    plt.ylabel('Unit Consumption')
+    plt.savefig('static/plot.png')
+
+    return render_template('index.html',
+    prediction_text= 'Monthly power consumption from {} to {} should be {}\n'.format(date_initial,date_final,prediction.resample('m').sum().values),
+    filename='plot.png')    
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
